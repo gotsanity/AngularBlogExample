f643d98a44233e2d4cdae18ea15ac3a35c79289a
jesse harlan <gotsanity@gmail.com>
2021-12-01 10:42:53 -0800

    finished edit component

--- src/app/blog-edit/blog-edit.component.ts
+++ src/app/blog-edit/blog-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { BlogService, BlogPost } from './../blog.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-blog-edit',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogEditComponent implements OnInit {
 
-  constructor() { }
+  @Output() onPostEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  blogForm: FormGroup = this.fb.group({
+    title: ['', Validators.required],
+    body: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder,
+    private blogService: BlogService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  clearForm(): void {
+    this.blogForm.setValue({
+      title: '',
+      body: ''
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.blogForm.value);
+    let post: BlogPost = {
+      title: this.blogForm.value.title,
+      body: this.blogForm.value.body,
+      id: "",
+      created_on: new Date(),
+      updated_on: undefined
+    };
+
+    this.blogService.createPost(post);
+    this.clearForm();
+    this.onPostEvent.emit(true);
   }
 
 }
